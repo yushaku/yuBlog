c2f480fb9220b58afb6348634d4a3a20d0c7040e
Yushaku <levanson180200@gmail.com>
2022-08-17 00:32:40 +0700

    add books pages and fix class name style

---
+++ pages/books/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Head from 'next/head'
+import BookItem from '../../components/books/BookItem'
+
+const BooksNote = () => {
+  return (
+    <div className="container px-4 mx-auto">
+      <Head>
+        <title>Books</title>
+      </Head>
+      <section className=" flex p-8 overflow-x-scroll scrollBar mt-[130px]">
+        <BookItem />
+        <BookItem />
+        <BookItem />
+        <BookItem />
+        <BookItem />
+        <BookItem />
+        <BookItem />
+        <BookItem />
+      </section>
+      <section className=" flex py-8 overflow-x-scroll scrollBar mt-[130px]">
+        <BookItem />
+        <BookItem />
+        <BookItem />
+        <BookItem />
+        <BookItem />
+        <BookItem />
+        <BookItem />
+        <BookItem />
+      </section>
+    </div>
+  )
+}
+
+export default BooksNote
