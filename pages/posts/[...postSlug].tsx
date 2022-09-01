f4ecb06d9ddaa6dc998ca06d1932aa51dbdf48f4
yushaku <levanson180200@gmail.com>
2022-09-02 00:30:27 +0700

    add PMA in my app

--- pages/posts/[...postSlug].tsx
+++ pages/posts/[...postSlug].tsx
@@ -4,6 +4,7 @@ import { useRouter } from 'next/router'
 import React, { useEffect, useState } from 'react'
 import Prism from 'prismjs'
 import Link from 'next/link'
+import { useAmp } from 'next/amp'
 import Head from 'next/head'
 import { AiOutlineDown } from 'react-icons/ai'
 import 'prismjs/themes/prism-tomorrow.css'
@@ -15,8 +16,11 @@ import RelatedPostList from '../../components/relatedPost/RelatedPostList'
 import CommentSection from '../../components/comment/CommentSection'
 import ReactSection from '../../components/article/ReactSection'
 
+export const config = { amp: 'hybrid' }
+
 const PostDetailPage = () => {
   const [postDetail, setPostDetail] = useState<PostDetail>()
+  const loadAmp = useAmp()
 
   const router = useRouter()
   const postSlug = router.query.postSlug?.[0] as string
@@ -48,45 +52,47 @@ const PostDetailPage = () => {
 
       {postDetail && (
         <div>
-          <div
-            style={{
-              backgroundImage: `url(${postDetail.featuredImage.url})`,
-              backgroundPosition: 'center',
-              backgroundSize: 'cover',
-            }}
-            className={`relative h-[100vh] w-[100vw] bg-cover bg-center`}
-          >
+          {loadAmp && (
             <div
-              className="absolute 
+              style={{
+                backgroundImage: `url(${postDetail.featuredImage.url})`,
+                backgroundPosition: 'center',
+                backgroundSize: 'cover',
+              }}
+              className={`relative h-[100vh] w-[100vw] bg-cover bg-center`}
+            >
+              <div
+                className="absolute 
               top-[40%] left-[5%] w-[90%] 
               md:left-[10%] md:w-[80%] 
               lg:top-[35%] lg:left-[20%] lg:w-[60%]"
-            >
-              <h1 className="py-4 text-[50px] dark:text-dark_accentColor text-light_textColor font-extrabold md:text-[60px] lg:text-[75px] shadow-inner">
-                {postDetail.title}
-              </h1>
-              <p className="text-[24px] text-light_textColor dark:text-dark_textColor md:text-[28px]">
-                {postDetail.excerpt}
-              </p>
+              >
+                <h1 className="py-4 text-[50px] dark:text-dark_accentColor text-light_textColor font-extrabold md:text-[60px] lg:text-[75px] shadow-inner">
+                  {postDetail.title}
+                </h1>
+                <p className="text-[24px] text-light_textColor dark:text-dark_textColor md:text-[28px]">
+                  {postDetail.excerpt}
+                </p>
+              </div>
+              <motion.div
+                className=" absolute text-4xl bottom-10 right-[50%] cursor-pointer"
+                animate={{
+                  y: [-10, 10],
+                  opacity: [100, 10],
+                }}
+                transition={{
+                  duration: 0.5,
+                  ease: 'easeInOut',
+                  repeat: Infinity,
+                  repeatDelay: 1,
+                }}
+              >
+                <Link href="#postSection">
+                  <AiOutlineDown />
+                </Link>
+              </motion.div>
             </div>
-            <motion.div
-              className=" absolute text-4xl bottom-10 right-[50%] cursor-pointer"
-              animate={{
-                y: [-10, 10],
-                opacity: [100, 10],
-              }}
-              transition={{
-                duration: 0.5,
-                ease: 'easeInOut',
-                repeat: Infinity,
-                repeatDelay: 1,
-              }}
-            >
-              <Link href="#postSection">
-                <AiOutlineDown />
-              </Link>
-            </motion.div>
-          </div>
+          )}
 
           <div id="postSection" className="p-4 max-w-[800px] mx-auto text-2xl mt-[70px]">
             {postDetail.content.raw.children.map((typeObj: any, index: number) => {
