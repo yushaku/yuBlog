c699e922cf7f4b357fd8acebc65c11162331ff19
son.lv <sonw.lv@zinza.com.vn>
2022-09-21 16:04:10 +0700

    update style in article list and card skeleton

--- components/skeleton/LoadingSkeletonPage.tsx
+++ components/skeleton/LoadingSkeletonPage.tsx
@@ -3,7 +3,7 @@ import CardSkeleton from './CardSkeleton'
 
 const LoadingSkeletonPage = () => {
   return (
-    <div className="container px-4 lg:px-[100px] mx-auto min-h-[65vh] mt-[12vh]">
+    <div className="container px-4 lg:px-[100px] mx-auto min-h-[65vh] mt-[20vh]">
       <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-[6rem] items-center">
         <CardSkeleton />
         <CardSkeleton />
