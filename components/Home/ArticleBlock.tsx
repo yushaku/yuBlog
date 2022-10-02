5600e665c14f160e1aeed716bc2b79ebf5fa9f8b
yushaku <levanson180200@gmail.com>
2022-10-02 20:50:16 +0700

    fix recommend post in home page

--- components/Home/ArticleBlock.tsx
+++ components/Home/ArticleBlock.tsx
@@ -1,25 +1,48 @@
 import React from 'react'
+
+import useGetPost from '@/hooks/useGetPost'
 import ArticleItem from './ArticleItem'
+import { articleItemProps } from '@/util/types/props'
 
 const ArticleBlock = () => {
+  const articleList: articleItemProps[] = useGetPost('article', 3)
+  const devList = useGetPost('code', 3)
+
   return (
     <div id="list_article" className="flex flex-col md:flex-row gap-8 my-[15vh]">
       <div id="Explore">
         <h1 className=" dark:text-dark_accentColor text-3xl">Article</h1>
         <p className=" border-t-4 border-red-400 w-[100px] py-4"></p>
         <ul className="">
-          <ArticleItem />
-          <ArticleItem />
-          <ArticleItem />
+          {articleList.map((itemArticle, index) => {
+            return (
+              <li key={index}>
+                <ArticleItem
+                  title={itemArticle.title}
+                  postSlug={itemArticle.postSlug}
+                  excerpt={itemArticle.excerpt as string}
+                />
+              </li>
+            )
+          })}
         </ul>
       </div>
-      <div id="Latest_Articles">
-        <h1 className=" dark:text-dark_accentColor text-3xl">developer</h1>
-        <p className=" border-t-4 border-red-400 w-[150px] py-4"></p>
-        <ul>
-          <ArticleItem />
-          <ArticleItem />
-          <ArticleItem />
+
+      <div id="Explore">
+        <h1 className=" dark:text-dark_accentColor text-3xl">developing</h1>
+        <p className=" border-t-4 border-red-400 w-[170px] py-4"></p>
+        <ul className="">
+          {devList.map((itemArticle, index) => {
+            return (
+              <li key={index}>
+                <ArticleItem
+                  title={itemArticle.title}
+                  postSlug={itemArticle.postSlug}
+                  excerpt={itemArticle.excerpt as string}
+                />
+              </li>
+            )
+          })}
         </ul>
       </div>
     </div>
