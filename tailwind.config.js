c7d6603f74c67720cf54e58a331744c77bc6a880
yushaku <levanson180200@gmail.com>
2022-10-02 20:55:39 +0700

    update loading page

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,6 +28,16 @@ module.exports = {
       boxShadow: {
         cardShadow: '-1rem 0 1rem #131313',
       },
+      keyframes: {
+        wave: {
+          '0%,40%, 100%': { transform: 'translateY(0)' },
+          '20%': { transform: 'translateY(-14px)' },
+        },
+      },
+      animation: {
+        wave: 'wave ease-in-out 1s infinite',
+        spin: 'spin 2s infinite',
+      },
     },
   },
   plugins: [require('tailwind-scrollbar-hide'), require('tailwind-scrollbar'), require('@tailwindcss/line-clamp')],
