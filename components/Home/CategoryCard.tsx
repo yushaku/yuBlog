9107b8bccd280fbc7f14bd376e031b643400c281
yushaku <levanson180200@gmail.com>
2022-10-10 00:10:04 +0700

    update icon

--- components/Home/CategoryCard.tsx
+++ components/Home/CategoryCard.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Link from 'next/link'
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const CategoryCard = ({ title, desc, link, icon }: any) => {
   return (
     <li className=" border-2 p-8 text-center rounded-lg max-w-[400px] cursor-pointer">
       <Link href={`/${link}`}>
         <div>
-          <i>{icon}</i>
+          <p>{icon}</p>
           <h2 className="text-4xl font-semibold dark:text-dark_accentColor my-2 ">{title}</h2>
           <p className="text-xl">{desc}</p>
         </div>
