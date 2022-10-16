bb32add85916c81686b742d33fff84fadf952105
yushaku <levanson180200@gmail.com>
2022-10-17 00:03:54 +0700

    update home book

--- components/Home/BookItem.tsx
+++ components/Home/BookItem.tsx
@@ -12,14 +12,16 @@ const BookItem = ({ title, description, imageLink, slug }: props) => {
   return (
     <Link href={`posts/${slug}`}>
       <div
-        className="flex items-center justify-start flex-col md:flex-row relative 
-        w-[300px] md:w-[400px] lg:w-[500px] dark:bg-dark_subBackground p-8 rounded-md gap-2 cursor-pointer"
+        className="relative
+        flex flex-col items-center justify-start gap-2 md:flex-row  
+        w-[300px] md:w-[400px] lg:w-[500px] 
+        dark:bg-dark_subBackground rounded-md cursor-pointer"
         id="book"
       >
-        <div className="w-[50%] absolute top-0 left-0 md:relative">
-          <Image src={imageLink} alt="image book" width={140} height={210} loading="lazy" />
+        <div className="absolute top-0 left-0 right-0 bottom-0 md:relative">
+          <Image src={imageLink} alt="image book" width={1000} height={1000} loading="lazy" />
         </div>
-        <div className="w-[70%]">
+        <div className="w-[100%] z-20 dark:bg-dark_background/80 md:w-[70%] p-8">
           <h2 className="text-xl md:text-2xl lg:text-3xl text-dark_accentColor">{title}</h2>
           <p className="text-lg md:text-lx line-clamp-5 mt-4">{description}</p>
         </div>
--- components/Home/BooksBlock.tsx
+++ components/Home/BooksBlock.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import Link from 'next/link'
-import BookItem from '@/components/Home/BookItem'
 import useGetPost from '@/hooks/useGetPost'
+import RelatedPostItem from '../relatedPost/RelatedPostItem'
 
 const BooksBlock = () => {
-  const bookList = useGetPost('books', 2)
+  const bookList = useGetPost('books', 3)
 
   return (
     <div id="bookBlock" className=" items-center text-center">
@@ -13,14 +12,13 @@ const BooksBlock = () => {
       <ul className="flex flex-col gap-8 justify-center items-center lg:flex-row">
         {bookList.map((book) => (
           <li key={book.id}>
-            <Link href={`posts/${book.postSlug}`}>
-              <BookItem
-                title={book.title}
-                description={book.excerpt as string}
-                imageLink={book?.featuredImage?.url}
-                slug={book.postSlug}
-              />
-            </Link>
+            <RelatedPostItem
+              id={book.id}
+              title={book.title}
+              featuredImage={book?.featuredImage}
+              postSlug={book.postSlug}
+              createdAt={book.createdAt}
+            />
           </li>
         ))}
       </ul>
