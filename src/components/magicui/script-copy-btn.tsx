73f7fd611742d2c7ce079399d99a8ec3d6a6217f
yushaku <levanson180200@gmail.com>
2025-05-02 22:53:26 +0700

    update UI

---
+++ src/components/magicui/script-copy-btn.tsx
@@ -0,0 +1,139 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { cn } from "@/utils/index";
+import { Check, Copy } from "lucide-react";
+import { motion } from "motion/react";
+import { useTheme } from "next-themes";
+import { HTMLAttributes, useEffect, useState } from "react";
+
+interface ScriptCopyBtnProps extends HTMLAttributes<HTMLDivElement> {
+  showMultiplePackageOptions?: boolean;
+  codeLanguage?: string;
+  lightTheme?: string;
+  darkTheme?: string;
+  commandMap: Record<string, string>;
+  className?: string;
+}
+
+export function ScriptCopyBtn({
+  showMultiplePackageOptions = true,
+  codeLanguage = "shell",
+  lightTheme = "catppuccin-mocha",
+  darkTheme = "catppuccin-macchiato",
+  commandMap,
+  className,
+}: ScriptCopyBtnProps) {
+  const packageManagers = Object.keys(commandMap);
+  const [packageManager, setPackageManager] = useState(packageManagers[0]);
+  const [copied, setCopied] = useState(false);
+  const [highlightedCode, setHighlightedCode] = useState("");
+  const { theme } = useTheme();
+  const command = commandMap[packageManager];
+
+  useEffect(() => {
+    async function loadHighlightedCode() {
+      try {
+        const { codeToHtml } = await import("shiki");
+        const highlighted = await codeToHtml(command, {
+          lang: codeLanguage,
+          themes: {
+            light: lightTheme,
+            dark: darkTheme,
+          },
+          defaultColor: theme === "dark" ? "dark" : "light",
+        });
+        setHighlightedCode(highlighted);
+      } catch (error) {
+        console.error("Error highlighting code:", error);
+        setHighlightedCode(`<pre>${command}</pre>`);
+      }
+    }
+
+    loadHighlightedCode();
+  }, [command, theme, codeLanguage, lightTheme, darkTheme]);
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(command);
+    setCopied(true);
+    setTimeout(() => setCopied(false), 2000);
+  };
+
+  return (
+    <div className={cn("max-w-md", className)}>
+      <div className='w-full space-y-2'>
+        <div className='mb-2 flex items-center justify-between'>
+          {showMultiplePackageOptions && (
+            <div className='relative'>
+              <div className='inline-flex overflow-hidden rounded-md translate-y-2.5 border text-xs'>
+                {packageManagers.map((pm, index) => (
+                  <div key={pm} className='flex items-center'>
+                    {index > 0 && (
+                      <div className='h-4 w-px bg-border' aria-hidden='true' />
+                    )}
+                    <Button
+                      variant='ghost'
+                      size='sm'
+                      className={`relative rounded-none bg-background px-2 py-1 hover:bg-background ${
+                        packageManager === pm
+                          ? "text-primary"
+                          : "text-muted-foreground"
+                      }`}
+                      onClick={() => setPackageManager(pm)}
+                    >
+                      {pm}
+                      {packageManager === pm && (
+                        <motion.div
+                          className='absolute inset-x-0 bottom-[1px] mx-auto h-0.5 w-[90%] bg-primary'
+                          layoutId='activeTab'
+                          initial={false}
+                          transition={{
+                            type: "spring",
+                            stiffness: 500,
+                            damping: 30,
+                          }}
+                        />
+                      )}
+                    </Button>
+                  </div>
+                ))}
+              </div>
+            </div>
+          )}
+        </div>
+
+        <div className='relative flex items-center'>
+          <div className='grow font-mono'>
+            {highlightedCode ? (
+              <div dangerouslySetInnerHTML={{ __html: highlightedCode }} />
+            ) : (
+              <pre className='rounded-md rounded-tl-none border border-border bg-foreground p-2 px-4 font-mono'>
+                {command}
+              </pre>
+            )}
+          </div>
+
+          <Button
+            variant='outline'
+            size='icon'
+            className='relative ml-2 rounded-md'
+            onClick={copyToClipboard}
+            aria-label={copied ? "Copied" : "Copy to clipboard"}
+          >
+            <span className='sr-only'>{copied ? "Copied" : "Copy"}</span>
+            <Copy
+              className={`h-4 w-4 transition-all duration-300 ${
+                copied ? "scale-0" : "scale-100"
+              }`}
+            />
+            <Check
+              className={`absolute inset-0 m-auto h-4 w-4 transition-all duration-300 ${
+                copied ? "scale-100" : "scale-0"
+              }`}
+            />
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/utils/mocs/evm.ts
+++ src/utils/mocs/evm.ts
@@ -8,7 +8,7 @@ const aa = {
       position: { x: 200, y: 0 },
       data: {
         label: "Account Abstraction",
-        link: "/AA_breakdown",
+        link: "/article/blockchain/AA_breakdown",
       },
     },
     {
@@ -17,13 +17,23 @@ const aa = {
       position: { x: 450, y: 0 },
       data: {
         label: "Erc-4337",
-        link: "/ERC-4337",
+        link: "/article/blockchain/ERC-4337",
+      },
+    },
+    {
+      id: "eip-7702",
+      type: "roadmap",
+      position: { x: 450, y: 100 },
+      data: {
+        label: "EIP-7702",
+        link: "/article/blockchain/eip-7702",
       },
     },
   ],
   edges: [
     { id: "a1", source: "ethereum", target: "aa", animated: false },
     { id: "a2", source: "aa", target: "erc-4337", animated: false },
+    { id: "a3", source: "aa", target: "eip-7702", animated: false },
   ],
 };
 
@@ -32,9 +42,18 @@ const indexer = {
     {
       id: "indexer",
       type: "roadmap",
-      position: { x: 200, y: 100 },
+      position: { x: 200, y: 200 },
       data: {
         label: "Indexer",
+        type: "main",
+      },
+    },
+    {
+      id: "the-graph",
+      type: "roadmap",
+      position: { x: 400, y: 200 },
+      data: {
+        label: "The Graph",
         type: "main",
       },
     },
@@ -44,6 +63,12 @@ const indexer = {
       id: "i1",
       source: "ethereum",
       target: "indexer",
+      animated: false,
+    },
+    {
+      id: "i2",
+      source: "indexer",
+      target: "the-graph",
       animated: false,
     },
   ],
